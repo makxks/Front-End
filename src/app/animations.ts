b53b6b0c6a93b77377a82c89731a738f743959da
Maximilian G. Platt <maxplatt7@gmail.com>
2019-03-04 19:51:42 +0800

    made angular animations optional, and changed colour of some text

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -20,17 +20,17 @@ export const slideInAnimation =
           left: 0,
           width: '100%'
         })
-      ]),
+      ], { optional: true }),
       query(':enter', [
         style({ top: '20em', opacity: '0' })
-      ]),
+      ], { optional: true }),
       group([
         query(':leave', [
           animate('500ms ease-out', style({ top: '20em', opacity: '0'}))
-        ]),
+        ], { optional: true }),
         query(':enter', [
           animate('500ms ease-out', style({ top: '0', opacity: '1'}))
-        ])
+        ], { optional: true })
       ]),
       query(':enter', animateChild()),
     ])
