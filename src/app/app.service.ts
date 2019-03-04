5c02f29497b4cf304239f440ce6de1b8b98d169d
Maximilian G. Platt <maxplatt7@gmail.com>
2019-03-04 15:53:19 +0800

    Add simple css theming

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -6,7 +6,26 @@ import { Injectable } from '@angular/core';
 
 export class AppService {
 
+  public theme: string = "light";
+
   constructor() {
+  }
+
+  switchTheme(){
+    if(this.theme == "light"){
+      this.theme = "dark";
+
+      var body = document.getElementById("body");
+      body.classList.add("dark");
+
+    }
+    else if(this.theme == "dark"){
+      this.theme = "light";
+
+      var body = document.getElementById("body");
+      body.classList.remove("dark");
+      
+    }
   }
 
 
